f28cfd6c3b0b016d2f1533f749915f93ad4f2375
Tamir <tamircher123@gmail.com>
2020-06-27 00:46:28 +0300

    fixed not showing single result in search + can run search from main

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import argparse
 from sdarot import SdarotPy
+from searchSerie import search
 
 MAX_EP_NUM = 50
 
@@ -7,27 +8,32 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--sid', type=int, required=True)
+    parser.add_argument('--sid', type=int)
     parser.add_argument('-s', '--seasons', type=str, default='1')
     parser.add_argument('-e', '--episodes', type=str)
+    parser.add_argument('--search', type=str)
 
     args = parser.parse_args()
 
-    season_split = args.seasons.split('-')
-    season_range = range(int(season_split[0]), int(season_split[-1]) + 1)
-
-    episode_range = []
-    if args.episodes is not None:
-
-        episode_split = args.episodes.split('-')
-        episode_range = range(
-            int(episode_split[0]), int(episode_split[-1]) + 1)
+    if args.search:
+        search(args.search)
     else:
-        episode_range = range(1, MAX_EP_NUM)
 
-    print(
-        f'Fetching Seasons {season_range[0]}-{season_range[-1]}, Episodes {episode_range[0]}-{episode_range[-1]}')
-    sdarot = SdarotPy(sid=args.sid, season_range=season_range,
-                      episode_range=episode_range)
+        season_split = args.seasons.split('-')
+        season_range = range(int(season_split[0]), int(season_split[-1]) + 1)
 
-    sdarot.download_series()
+        episode_range = []
+        if args.episodes is not None:
+
+            episode_split = args.episodes.split('-')
+            episode_range = range(
+                int(episode_split[0]), int(episode_split[-1]) + 1)
+        else:
+            episode_range = range(1, MAX_EP_NUM)
+
+        print(
+            f'Fetching Seasons {season_range[0]}-{season_range[-1]}, Episodes {episode_range[0]}-{episode_range[-1]}')
+        sdarot = SdarotPy(sid=args.sid, season_range=season_range,
+                          episode_range=episode_range)
+
+        sdarot.download_series()
--- searchSerie.py
+++ searchSerie.py
@@ -2,23 +2,45 @@ import requests
 from lxml import html
 import sys
 
-
+title_single_res_path = "//div[@class='poster']//h1/strong/text()"
 element_xpath = "//div[contains(@class,'sInfo')]"
 english_title_xpath = "./div/h5"
 code_xpath = "./a"
 
-term = sys.argv[1]
-### search term ###
-res = requests.get(f'https://sdarot.today/search?term={term}')
-tree = html.fromstring(res.content)
-elements = tree.xpath(element_xpath)
 
-for element in elements:
+def search(term):
 
-    title = element.xpath(english_title_xpath)[0].text
+    print(f'Searching for: {term}')
+    ### search term ###
+    res = requests.get(f'https://sdarot.today/search?term={term}')
+    tree = html.fromstring(res.content)
 
-    # get serie code from img src url
-    serie_code = element.xpath(code_xpath)[0].get(
-        'href').rsplit('/', 1)[1].split('-', 1)[0]
+    res_url = res.url
+    if res_url.find('/watch/') != -1:
 
-    print(f'{title} - {serie_code}')
+        title = tree.xpath(title_single_res_path)[0].split(' / ')[1]
+        serie_code = res_url.split('/watch/', 1)[1].split('-', 1)[0]
+
+        print(f'{title} - {serie_code}\n')
+
+        print(f'Run this to download season 1 of {title}:')
+        print(f'python main.py --sid={serie_code} -s=1')
+    else:
+
+        elements = tree.xpath(element_xpath)
+
+        for element in elements:
+
+            title = element.xpath(english_title_xpath)[0].text
+
+            # get serie code from img src url
+            serie_code = element.xpath(code_xpath)[0].get(
+                'href').rsplit('/', 1)[1].split('-', 1)[0]
+
+            print(f'{title} - {serie_code}')
+
+
+if __name__ == "__main__":
+
+    term = ' '.join(sys.argv[1:])
+    search(term)
